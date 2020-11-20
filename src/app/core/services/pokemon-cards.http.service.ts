702fc10bae8dfbd26b79fd0427290147a2773de8
MaciejNawrot <wafelweedjr@gmail.com>
2020-11-20 22:59:01 +0100

    interfaces barrel moduled

--- src/app/core/services/pokemon-cards.http.service.ts
+++ src/app/core/services/pokemon-cards.http.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PokemonDetailResponse, PokemonListResponse } from '../interfaces/pokemons.interfaces';
-import { environment} from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
